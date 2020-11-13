625fa1e61d7aec09eaf7d3f0459c579b54795c20
Jorge Contreras <jrcr23@gmail.com>
2020-11-12 22:08:12 -0500

    Update next_larger.py
    Add better solution using stack

--- next_larger.py
+++ next_larger.py
@@ -1,4 +1,7 @@
-def nextLarger(a):
+
+# brute-froce solution O(n^2)
+
+def nextLargerSlow(a):
     for i, n in enumerate(a):
         swap = False
         for j in range(i+1, len(a)):
@@ -12,3 +15,42 @@ def nextLarger(a):
     
     a[len(a) - 1] = -1
     return a
+
+
+# using stack O(n)
+
+class Stack():
+    def __init__(self):
+        self.size = 0
+        self.items = []
+
+    def push(self, value):
+        self.items.append(value)
+        self.size += 1
+
+    def pop(self):
+        if self.size > 0:
+            item = self.items.pop()
+            self.size -= 1
+            return item
+        return None
+
+    def peek(self):
+        if self.size > 0:
+            return self.items[self.size - 1]
+        return None
+
+
+def nextLargerFast(a):
+    n = len(a)
+    ans = [-1 for _ in range(n)]
+    stack = Stack()
+    stack.push(0)
+
+    for i in range(1, n):
+        while stack.size > 0 and a[stack.peek()] < a[i]:
+            ans[stack.peek()] = a[i]
+            stack.pop()
+        stack.push(i)
+    
+    return ans
