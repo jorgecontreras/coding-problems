10176427dd34c8a58e5275277420a78f4918a5af
jorge <jrcr23@gmail.com>
2021-01-13 15:00:10 -0500

    Counting bits using bitwise AND operator

---
+++ count_bits.py
@@ -0,0 +1,13 @@
+# count bits
+
+def count_bits(n):
+    c = 0
+    while n:
+        n &= n - 1
+        c += 1
+    return c
+
+assert count_bits(0b11001100) == 4
+assert count_bits(0b00000000000000000000000000001011) == 3
+assert count_bits(0b00000000000000000000000010000000) == 1
+assert count_bits(0b11111111111111111111111111111101) == 31
