7707d07b5409f2413897ced1c1d3f62e9709db51
jorge <jrcr23@gmail.com>
2021-01-31 17:10:35 -0500

    cyclic sort updates

--- patterns/cyclic-sort/cyclic-sort.py
+++ patterns/cyclic-sort/cyclic-sort.py
@@ -1,19 +1,40 @@
 # cyclic-sort
 
-# You are given an unsorted array containing numbers from the range 1 to n. 
-# The array can have duplicates, which means that some numbers will be missing. 
-# Find all the missing numbers.
+"""
+We are given an array containing ‘n’ objects. 
+Each object, when created, was assigned a unique number from 1 to ‘n’ based on their creation sequence. 
+This means that the object with sequence number ‘3’ was created just before the object with sequence number ‘4’.
+
+Write a function to sort the objects in-place on their creation sequence number in O(n)O(n) and without any extra space. 
+For simplicity, let’s assume we are passed an integer array containing only the sequence numbers, though each number is actually an object.
+
+Example 1:
+
+Input: [3, 1, 5, 4, 2]
+Output: [1, 2, 3, 4, 5]
+Example 2:
+
+Input: [2, 6, 4, 3, 1, 5]
+Output: [1, 2, 3, 4, 5, 6]
+Example 3:
+
+Input: [1, 5, 6, 4, 3, 2]
+Output: [1, 2, 3, 4, 5, 6]
+"""
 
 def cyclic_sort(nums):
-    for i in range(len(nums)):
-        while nums[i] != i+1:
-            swp = nums[i]
-            nums[i] = nums[nums[i] - 1]
-            nums[swp - 1] = swp
+    i = 0
+    while i < len(nums):
+        j = nums[i] - 1
+        if nums[i] != nums[j]:
+            nums[i], nums[j] = nums[j], nums[i]
+        else:
+            i += 1
 
     return nums
 
-arr = [3,1,5,4,2]
+
+arr = [1, 5, 6, 4, 3, 2]
 
 answer = cyclic_sort(arr)
 print(answer)
