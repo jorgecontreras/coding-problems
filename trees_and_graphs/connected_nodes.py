5adff7b02ee46819b0d662caa6b96baa7dc88808
jorge <jrcr23@gmail.com>
2021-02-28 19:42:02 -0500

    Connected nodes: bfs

---
+++ trees_and_graphs/connected_nodes.py
@@ -0,0 +1,54 @@
+# Find connected nodes BFS 
+
+from collections import deque, defaultdict
+
+def find_connected(edges):
+    graph = defaultdict(list)
+
+    # build the undirected graph - add both edges
+    for e0, e1 in edges:
+        graph[e0].append(e1)
+        graph[e1].append(e0)
+
+    # find the connected nodes
+    visited = set()
+    components = []
+
+    for node in graph:
+        queue = deque([node])
+        component = []
+        while queue:
+            size = len(queue)
+            for _ in range(size):
+                n = queue.popleft()
+                if n in visited:
+                    continue
+                visited.add(n)
+                component.append(n)
+                queue += graph[n]
+
+        if component:
+            components.append(component)
+
+    return components
+            
+
+
+
+
+edges = [
+    [1,0],
+    [1,8],
+    [4,5],
+    [6,2],
+    [2,4],
+    [5,2]
+]
+
+groups = find_connected(edges)
+
+
+[1,0,8]
+[4,5,2,6]
+
+print(groups)
