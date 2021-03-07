4008c582f5fee807baffddcbf3483eb1f839334c
jorge <jrcr23@gmail.com>
2021-03-07 11:28:44 -0500

    Binary Tree: Flatten

--- trees_and_graphs/binary_tree_flatten.py
+++ trees_and_graphs/binary_tree_flatten.py
@@ -1,4 +1,15 @@
-# binary tree flatten to linked list
+# Binary tree flatten to linked list
+#
+# LeetCode 114 https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+#
+# Given the root of a binary tree, flatten the tree into a "linked list":
+#
+# The "linked list" should use the same TreeNode class where the right child pointer points 
+# to the next node in the list and the left child pointer is always null.
+# 
+# The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+#
+
 class Node:
     def __init__(self, val):
         self.val = val
@@ -7,7 +18,7 @@ class Node:
 
 class Solution:
 
-    def flatten_r(self, node):
+    def flatten_recursive(self, node):
 
         if not node:
             return None 
@@ -15,10 +26,10 @@ class Solution:
         if not node.right and not node.left:
             return node
 
-        left_tail = self.flatten_r(node.left)
-        right_tail = self.flatten_r(node.right)
+        left_tail = self.flatten_recursive(node.left)
+        right_tail = self.flatten_recursive(node.right)
 
-        # shuffle pointers
+        # shuffle pointers: this is the flatten
         if left_tail:
             left_tail.right = node.right 
             node.right = node.left
@@ -26,17 +37,19 @@ class Solution:
 
         return right_tail if right_tail else left_tail
 
-    def show(self, node):
+    def check(self, node):
+        result = []
 
-        print(node.val)
+        result.append(node.val)
         while node.right:
-            print(node.left)
-            print(node.right.val)
+            result.append(node.right.val)
             node = node.right
+
+        return result
             
 
     def flatten(self, root):
-        self.flatten_r(root)
+        self.flatten_recursive(root)
 
 
 n = Node(1)
@@ -49,4 +62,6 @@ n.right.right = Node(6)
 s = Solution()
 
 s.flatten(n)
-s.show(n)
+assert s.check(n) == [1,2,3,4,5,6]
+
+print("all tests passed.")
