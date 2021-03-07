e370d4390bc61826dbd835495cb3c62e314e607a
jorge <jrcr23@gmail.com>
2021-03-07 17:44:56 -0500

    recursion: encrypted words

--- recursion_backtracking/encrypted_words.py
+++ recursion_backtracking/encrypted_words.py
@@ -1,16 +1,20 @@
-# encrypted words
+# ENCRYPTED WORDS
+#
+# You've devised a simple encryption method for alphabetic strings that shuffles the characters in such a way that the resulting string is hard to quickly read, but is easy to convert back into the original string.
+# When you encrypt a string S, you start with an initially-empty resulting string R and append characters to it as follows:
 
-# ex. a, b, c : bac
-# 
-# ex. abcxcba: xbacbca
+# Append the middle character of S (if S has even length, then we define the middle character as the left-most of the two central characters)
+# Append the encrypted version of the substring of S that's to the left of the middle character (if non-empty)
+# Append the encrypted version of the substring of S that's to the right of the middle character (if non-empty)
 
-# mid = 3
-# 
-# abc
-# mid = 1
-# a
-# time complexity O(NlogN)
-# space complexity: O(1)
+# For example, to encrypt the string "abc", we first take "b", and then append the encrypted version of "a" (which is just "a") and the encrypted version of "c" (which is just "c") to get "bac".
+# If we encrypt "abcxcba" we'll get "xbacbca". That is, we take "x" and then append the encrypted version "abc" and then append the encrypted version of "cba".
+#
+# SOLUTION:
+# Recursion
+#
+# Time complexity O(NlogN)
+# Space complexity: O(1)
 
 def encrypt(text):
 
@@ -18,17 +22,19 @@ def encrypt(text):
     if len(text) < 2:
         return text
 
-    start, end = 0, len(text) - 1
-    mid = start + (end-start) // 2
+    mid = (len(text) - 1) // 2
 
-    left = encrypt(text[start:mid])
-    right = encrypt(text[mid+1:])
+    return text[mid] + encrypt(text[0:mid]) + encrypt(text[mid+1:])
 
-    return text[mid] + left + right
-
+# tests
+t1 = "abc"
+expected_1 = "bac"
+output_1 = encrypt(t1)
 
 t2 = "abcxcba"
 expected_2 = "xbacbca"
 output_2 = encrypt(t2)
 
+assert expected_1 == output_1
 assert expected_2 == output_2
+print("all tests passed.")
