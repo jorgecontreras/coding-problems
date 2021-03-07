4ef2924802a1d33d7896dd584654d4325c436c2a
jorge <jrcr23@gmail.com>
2021-03-07 11:14:41 -0500

    BST: is valid

--- trees_and_graphs/validate_bst.py
+++ trees_and_graphs/validate_bst.py
@@ -1,11 +1,14 @@
+import math
+
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class Node:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
-    def isValidBST(self, root: TreeNode) -> bool:
+    def isValidBST(self, root: Node) -> bool:
         
         #in order traversal
         def traverse(root):
@@ -27,3 +30,21 @@ class Solution:
         
         self.prev = -math.inf
         return traverse(root)
+
+s = Solution()
+
+n = Node(2)
+n.left = Node(1)
+n.right = Node(3)
+
+assert s.isValidBST(n) == True
+
+n = Node(5)
+n.left = Node(1)
+n.right = Node(4)
+n.right.left = Node(3)
+n.right.right = Node(6)
+
+assert s.isValidBST(n) == False
+
+print("all tests passed.")
