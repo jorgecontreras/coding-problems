f896e76bfb048d158caa534b45411a4422a40b27
jorge <jrcr23@gmail.com>
2021-03-07 12:13:15 -0500

    arrays: move zeroes add description

--- arrays_and_strings/move_zeroes.py
+++ arrays_and_strings/move_zeroes.py
@@ -1,8 +1,17 @@
 # move zeroes
+#
+# Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+# Example:
+
+# Input: [0,1,0,3,12]
+# Output: [1,3,12,0,0]
+# Note:
 
 # You must do this in-place without making a copy of the array.
 # Minimize the total number of operations.
-
+#
+# Solution: in reality, we are moving non-zeroes to the left, resulting in all zeroes on the right
 
 from collections import deque
 
@@ -19,4 +28,7 @@ class Solution:
 
 t1 = [0,1,0,3,12]
 s = Solution()
-print(s.goodMove(t1))
+ans = s.goodMove(t1)
+
+assert t1 == [1, 3, 12, 0, 0]
+print("all tests passed.")
