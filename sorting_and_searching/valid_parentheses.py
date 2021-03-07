0caac832ea9b5a53ab7b004fa88e783462c7f1d3
jorge <jrcr23@gmail.com>
2021-03-07 17:28:24 -0500

    stacks: balanced brackets - valid parentheses

--- sorting_and_searching/valid_parentheses.py
+++ sorting_and_searching/valid_parentheses.py
@@ -1,3 +1,24 @@
+"""
+VALID PARENTHESIS - BALANCED BRACKETS
+
+A bracket is considered to be any one of the following characters: (, ), {, }, [, or ].
+We consider two brackets to be matching if the first element is an open-bracket, e.g., (, {, or [, and the second bracket is a close-bracket of the same type, e.g., ( and ), [ and ], and { and } are the only pairs of matching brackets.
+Furthermore, a sequence of brackets is said to be balanced if the following conditions are met:
+
+    - The sequence is empty, or
+    - The sequence is composed of two, non-empty, sequences both of which are balanced, or
+    - The first and last brackets of the sequence are matching, and the portion of the sequence without the first and last elements is balanced.
+
+You are given a string of brackets. Your task is to determine whether each sequence of brackets is balanced. If a string is balanced, return true, otherwise, return false
+"""
+
+# Solution: 
+#
+# Use a stack. Add opening brackets to the stack. When a closing bracket is found, the top of the stack must be the matching bracket.
+#
+# Time complexity: O(N). Iterate the string one character at a time, for the length of the string.
+# Space complexity: O(N). To store the brackets in a stack, worst case would be that we have only opening brackets.
+
 def valid_parentheses(s):
     matches = {
         ")": "(",
