171d549d3a5964ec0e05a5af8ccfd45d4ecf9d49
jorge <jrcr23@gmail.com>
2021-03-09 14:13:53 -0500

    checkpoint: product except self

--- arrays_and_strings/product_except_self.py
+++ arrays_and_strings/product_except_self.py
@@ -1,4 +1,20 @@
-# product except self
+# Product except self
+#
+# LeetCode 238 https://leetcode.com/problems/product-of-array-except-self/
+# Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+# Example:
+
+# Input:  [1,2,3,4]
+# Output: [24,12,8,6]
+
+# Solution:
+# Get the products in two passes. 
+# first pass: products to the left
+# second pass: products to the right
+#
+# Time complexity: O(N)
+# Space complexity: O(N)
 
 def product_except_self(nums):
     result = [1]
@@ -9,7 +25,7 @@ def product_except_self(nums):
         p *= nums[i-1]
         result.append(p)
     
-    # second pass: get the product o the right-side elements and update the result array
+    # second pass: get the product of the right-side elements and update the result array
     p = 1
     for i in range(len(nums)-1, -1, -1):
         result[i] *= p
