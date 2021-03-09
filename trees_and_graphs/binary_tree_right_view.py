dd6e3c4506af571d4e0369e02eb6e6d3302039f1
jorge <jrcr23@gmail.com>
2021-03-09 13:47:08 -0500

    binary tree right view tests

--- trees_and_graphs/binary_tree_right_view.py
+++ trees_and_graphs/binary_tree_right_view.py
@@ -1,6 +1,11 @@
 # binary tree right side view
-
 from collections import deque
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
 
 def right_view(root):
 
@@ -26,3 +31,29 @@ def right_view(root):
                 queue.append(node.right)
 
     return result        
+
+# tests
+n = Node(3)
+n.left = Node(4)
+n.right = Node(4)
+n.left.left = Node(7)
+n.left.right = Node(1)
+n.right.left = Node(1)
+n.right.right = Node(7)
+assert right_view(n) == [3,4,7]
+
+n = Node(3)
+assert right_view(n) == [3]
+
+n = Node(3)
+n.left = Node(4)
+n.right = Node(4)
+n.left.left = Node(7)
+n.left.right = Node(1)
+n.right.left = Node(1)
+n.right.right = Node(7)
+n.left.right.left = Node(0)
+
+assert right_view(n) == [3,4,7,0]
+
+print("all tests passed.")
