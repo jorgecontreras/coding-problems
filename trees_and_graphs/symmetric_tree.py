37f7ef1609ec0d3042e3e3b869f775de2ff5566f
jorge <jrcr23@gmail.com>
2021-03-09 12:16:11 -0500

    symmetric tree tests

--- trees_and_graphs/symmetric_tree.py
+++ trees_and_graphs/symmetric_tree.py
@@ -5,6 +5,12 @@
 # Time complexity: O(n), because we traverse the tree once
 # Space complexity: O(n), for the call stack, being the worst case a linear tree.
 
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
 def is_symmetric(root):
     if not root:
         return True
@@ -16,3 +22,30 @@ def is_mirror(left, right):
         return left.val == right.val and is_mirror(left.left, right.right) and is_mirror(left.right, right.left)
     
     return left is right
+
+# tests
+n = Node(3)
+n.left = Node(4)
+n.right = Node(4)
+n.left.left = Node(7)
+n.left.right = Node(1)
+n.right.left = Node(1)
+n.right.right = Node(7)
+
+assert is_symmetric(n) == True
+
+n = Node(3)
+n.left = Node(4)
+n.right = Node(4)
+n.left.left = Node(7)
+n.left.right = Node(8)
+n.right.left = Node(1)
+n.right.right = Node(7)
+
+assert is_symmetric(n) == False
+
+n = Node(3)
+
+assert is_symmetric(n) == True
+
+print("all tests passed.")
