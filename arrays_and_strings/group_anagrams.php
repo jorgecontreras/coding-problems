0c9b841288777cf226b37386f4e982da410fd9d4
jorgecontreras <jrcr23@gmail.com>
2021-03-25 22:37:04 -0400

    Arrays: using arrays as dictionaries in PHP

---
+++ arrays_and_strings/group_anagrams.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+group anagrams
+*/
+
+class Solution 
+{
+
+    public function group_anagrams($words) {
+        $groups = array();
+
+        foreach ($words as $word) {
+            $chars = str_split($word);
+            sort($chars);
+            $key = implode($chars);
+
+            if(!isset($groups[$key])) {
+                $groups[$key] = array();
+            }
+            $groups[$key][] = $word;
+        }
+
+        return $groups;
+
+    }
+
+}
+
+
+$words = ["cab","cod","tin","pew","duh","may","ill","buy","int","bar","max","doc", "nit", "same", "mase", "ames"];
+
+$s = new Solution();
+$anagrams = $s->group_anagrams($words);
+
+
+foreach ($anagrams as $key => $words) {
+    echo $key . ": [";
+    echo implode(", ", $words);
+    echo "]\n";
+}
+
+?>
